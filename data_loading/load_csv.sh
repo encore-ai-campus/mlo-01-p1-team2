eed2cd24b2698949bf9c3e4a42a29a7cc9b16f3c
BSSM <wls3983@gmail.com>
2026-08-12 15:19:22 +0900

    upsert update

--- data_loading/load_csv.sh
+++ data_loading/load_csv.sh
@@ -4,6 +4,7 @@ set -e
 
 DB_NAME="projectTest"
 TABLE_NAME="cars"
+STAGING_TABLE="cars_staging"
 CSV_PATH="/home/playdata/test/cars.csv"
 
 if [ ! -f "$CSV_PATH" ]; then
@@ -15,13 +16,37 @@ echo "CSV 적재 시작: $CSV_PATH"
 
 mysql "$DB_NAME" <<SQL
 
+TRUNCATE TABLE $STAGING_TABLE;
+
 LOAD DATA LOCAL INFILE '$CSV_PATH'
-INTO TABLE $TABLE_NAME
+INTO TABLE $STAGING_TABLE
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+INSERT INTO $TABLE_NAME (
+    listing_number,
+    title,
+    price,
+    mileage_km,
+    status
+)
+SELECT
+    listing_number,
+    title,
+    price,
+    mileage_km,
+    status
+FROM $STAGING_TABLE
+AS new
+ON DUPLICATE KEY UPDATE
+    listing_number = new.listing_number,
+    title = new.title,
+    price = new.price,
+    mileage_km = new.mileage_km,
+    status = new.status;
 
 SELECT COUNT(*) AS total_rows
 FROM $TABLE_NAME;
